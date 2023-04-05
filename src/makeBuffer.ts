7b9eab1f6593dad6c97a260ce444e8e2fac94b24
dadamssg <adams.david.10@gmail.com>
2023-04-04 22:53:30 -0500

    test: add testing

---
+++ src/makeBuffer.ts
@@ -0,0 +1,22 @@
+import { InterceptFn, Sheet } from "./types"
+
+export default async function makeBuffer<T extends Array<Sheet>>({
+  worksheets,
+  data,
+  intercept,
+}: {
+  worksheets: T
+  data: Array<any> | Record<T[number]["name"], Array<any>>
+  intercept?: InterceptFn
+}) {
+  const ExcelJS = await import("exceljs")
+  let workbook = new ExcelJS.Workbook()
+  for (const worksheet of worksheets) {
+    const sheet = workbook.addWorksheet(worksheet.name)
+    sheet.columns = worksheet.columns
+    const rows = (Array.isArray(data) ? data : data[worksheet.name as T[number]["name"]]) ?? []
+    sheet.addRows(rows)
+  }
+  workbook = intercept ? intercept(workbook) ?? workbook : workbook
+  return await workbook.xlsx.writeBuffer()
+}
---
+++ test/makeBuffer.test.ts
@@ -0,0 +1,105 @@
+import { expect, test } from "vitest"
+import ExcelJS from "exceljs"
+import makeBuffer from "../src/makeBuffer"
+
+const worksheetA = {
+  name: "Sheet 1",
+  columns: [
+    {
+      header: "Id",
+      key: "id",
+      width: 10,
+    },
+    {
+      header: "Name",
+      key: "name",
+      width: 32,
+    },
+    {
+      header: "D.O.B.",
+      key: "dob",
+      width: 200,
+    },
+  ],
+}
+
+const worksheetB = {
+  name: "Cities",
+  columns: [
+    {
+      header: "City",
+      key: "city",
+    },
+  ],
+}
+
+test("can create one sheet", async () => {
+  const buffer =  await makeBuffer({
+    data: [
+      { id: 1, name: "Jane Doe", dob: new Date(1984, 6, 7) },
+      { id: 2, name: "John Doe", dob: new Date(1965, 1, 7) },
+    ],
+    worksheets: [worksheetA],
+  })
+
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.load(buffer);
+  const worksheet = workbook.getWorksheet(worksheetA.name)
+
+  expect(worksheet.getCell('A1').text).toBe('Id')
+  expect(worksheet.getCell('A2').text).toBe('1')
+  expect(worksheet.getCell('A3').text).toBe('2')
+})
+
+test("can create two sheets", async () => {
+  const buffer =  await makeBuffer({
+    data: {
+      [worksheetA.name]: [
+        { id: 1, name: "Jane Doe", dob: new Date(1984, 6, 7) },
+        { id: 2, name: "John Doe", dob: new Date(1965, 1, 7) },
+      ],
+      [worksheetB.name]: [
+        { city: 'Dallas' },
+        { city: 'New York' },
+        { city: 'Miami' },
+      ]
+    },
+    worksheets: [worksheetA, worksheetB],
+  })
+
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.load(buffer);
+  const sheetA = workbook.getWorksheet(worksheetA.name)
+
+  expect(sheetA.getCell('A1').text).toBe('Id')
+  expect(sheetA.getCell('A2').text).toBe('1')
+  expect(sheetA.getCell('A3').text).toBe('2')
+
+  const sheetB = workbook.getWorksheet(worksheetB.name)
+
+  expect(sheetB.getCell('A1').text).toBe('City')
+  expect(sheetB.getCell('A2').text).toBe('Dallas')
+  expect(sheetB.getCell('A3').text).toBe('New York')
+  expect(sheetB.getCell('A4').text).toBe('Miami')
+})
+
+test("can use intercept", async () => {
+  const buffer =  await makeBuffer({
+    data: [
+      { id: 1, name: "Jane Doe", dob: new Date(1984, 6, 7) },
+      { id: 2, name: "John Doe", dob: new Date(1965, 1, 7) },
+    ],
+    worksheets: [worksheetA],
+    intercept: (workbook) => {
+      workbook.getWorksheet(worksheetA.name).getCell('A2').value = 'foobar'
+    }
+  })
+
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.load(buffer);
+  const worksheet = workbook.getWorksheet(worksheetA.name)
+
+  expect(worksheet.getCell('A1').text).toBe('Id')
+  expect(worksheet.getCell('A2').text).toBe('foobar')
+  expect(worksheet.getCell('A3').text).toBe('2')
+})
