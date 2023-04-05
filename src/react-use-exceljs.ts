28397bc531c585a73fc364b75490e6222957fe3d
dadamssg <adams.david.10@gmail.com>
2023-04-04 21:16:31 -0500

    fix: memo download

--- src/react-use-exceljs.ts
+++ src/react-use-exceljs.ts
@@ -1,5 +1,6 @@
 import { type Workbook, type Worksheet } from "exceljs"
 import { saveAs } from "file-saver"
+import React from "react"
 
 type Sheet = { name: string; columns: Worksheet["columns"] }
 type InterceptFn = (workbook: Workbook) => Workbook | void
@@ -18,20 +19,23 @@ export default function useExcelJS<T extends Array<Sheet>>({
   type Data = Array<any> | Record<WorksheetName, Array<any>>
 
   return {
-    download: async (data: Data) => {
-      const ExcelJS = await import("exceljs")
-      let workbook = new ExcelJS.Workbook()
-      for (const worksheet of worksheets) {
-        const sheet = workbook.addWorksheet(worksheet.name)
-        sheet.columns = worksheet.columns
-        const rows = (Array.isArray(data) ? data : data[worksheet.name as WorksheetName]) ?? []
-        sheet.addRows(rows)
-      }
-      workbook = intercept ? intercept(workbook) ?? workbook : workbook
-      const buffer = await workbook.xlsx.writeBuffer()
-      const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-      const blob = new Blob([buffer], { type: fileType })
-      saveAs(blob, filename ?? "workbook.xlsx")
-    },
+    download: React.useCallback(
+      async (data: Data) => {
+        const ExcelJS = await import("exceljs")
+        let workbook = new ExcelJS.Workbook()
+        for (const worksheet of worksheets) {
+          const sheet = workbook.addWorksheet(worksheet.name)
+          sheet.columns = worksheet.columns
+          const rows = (Array.isArray(data) ? data : data[worksheet.name as WorksheetName]) ?? []
+          sheet.addRows(rows)
+        }
+        workbook = intercept ? intercept(workbook) ?? workbook : workbook
+        const buffer = await workbook.xlsx.writeBuffer()
+        const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        const blob = new Blob([buffer], { type: fileType })
+        saveAs(blob, filename ?? "workbook.xlsx")
+      },
+      [filename, worksheets, intercept]
+    ),
   }
 }
